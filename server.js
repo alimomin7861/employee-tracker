28259daf3fc04be9014f97b72df7648e0a18a04d
Ali Momin <Ali@Alis-MBP-2.attlocal.net>
2023-04-17 18:19:53 -0400

    added in all the add functions to server.js

--- server.js
+++ server.js
@@ -87,4 +87,92 @@ function viewEmployees() {
         })
 }
 
+function addDepartment() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'departmentName',
+                message: 'What is the name of the department?'
+            }
+        ]).then(response => {
+            const query = `INSERT INTO department SET ?`
+            db.query(
+                query, {
+                department_name: response.departmentName
+            }
+            )
+            console.log(`${response.departmentName} department has been added to the database`);
+            start()
+        })
+}
 
+function addRole() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'roleName',
+                message: 'What is the name of the role?'
+            },
+            {
+                type: 'input',
+                name: 'roleSalary',
+                message: 'What is the salary of the role?'
+            },
+            {
+                type: 'input',
+                name: 'roleDepartment',
+                message: 'Which department does the role belong to?'
+            },
+        ]).then(response => {
+            const query = `INSERT INTO role SET ?`
+            db.query(
+                query, {
+                title: response.roleName,
+                salary: response.roleSalary,
+                department_id: response.roleDepartment
+            }
+            )
+            console.log(`${response.roleName} role has been added to the database`);
+            start()
+        })
+}
+
+function addEmployee() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'firstName',
+                message: 'What is the first name of the employee?'
+            },
+            {
+                type: 'input',
+                name: 'lastName',
+                message: 'What is the last name of the employee?'
+            },
+            {
+                type: 'input',
+                name: 'employeeRole',
+                message: "What is the employee's role ID ?"
+            },
+            {
+                type: 'input',
+                name: 'employeeManager',
+                message: "Who is the employee's manager?"
+            },
+        ]).then(response => {
+            const query = `INSERT INTO role SET ?`
+            db.query(
+                query, {
+                first_name: respomse.firstName,
+                last_name: respomse.lastName,
+                role_id: response.employeeRole,
+                manager_id: response.employeeManager
+            }
+            )
+            console.log(`${response.firstName} ${response.lastName} has been added to the database`);
+            start()
+        })
+}
